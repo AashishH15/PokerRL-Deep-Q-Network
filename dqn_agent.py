1c339e31dd979b2649618bee27b9eb9c92528516
Aashish Harishchandre <aashish0415@gmail.com>
2026-05-30 12:35:34 -0400

    optimize batch unpacking and tensor conversion in dqn_agent training step

--- dqn_agent.py
+++ dqn_agent.py
@@ -92,18 +92,15 @@ class DQNAgent:
             return
             
         batch, indices, weights = self.prioritized_sample(Config.BATCH_SIZE)
-        states, actions, rewards, next_states, dones = zip(*batch)
-        hand_strengths = torch.FloatTensor([
-            self.get_hand_strength(state) for state in states
-        ]).to(self.device)
-        next_hand_strengths = torch.FloatTensor([
-            self.get_hand_strength(state) for state in next_states
-        ]).to(self.device)
+        states, hand_strengths, actions, rewards, next_states, next_hand_strengths, dones = zip(*batch)
         
-        states = torch.FloatTensor(states).to(self.device)
+        hand_strengths = torch.FloatTensor(hand_strengths).to(self.device)
+        next_hand_strengths = torch.FloatTensor(next_hand_strengths).to(self.device)
+        
+        states = torch.FloatTensor(np.array(states)).to(self.device)
         actions = torch.LongTensor(actions).to(self.device)
         rewards = torch.FloatTensor(rewards).to(self.device)
-        next_states = torch.FloatTensor(next_states).to(self.device)
+        next_states = torch.FloatTensor(np.array(next_states)).to(self.device)
         dones = torch.FloatTensor(dones).to(self.device)
         weights = torch.FloatTensor(weights).to(self.device)
         
